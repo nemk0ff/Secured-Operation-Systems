30a84c1b882399330b14ffd81eca203f2c97e9eb
Nemkoff <nemkov_work@mail.ru>
2025-10-09 16:07:17 +0300

    remake lab3

--- lab3_fork/main.c
+++ lab3_fork/main.c
@@ -1,113 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <errno.h>
-#include <error.h>
-#include <string.h>
 #include <signal.h>
+#include <string.h>
 
-void print_pid() {
-    pid_t self = getpid();
-    pid_t parent = getppid();
-
-    printf("[%d] Parent: %d\n", self, parent);
+void process_exit_handler(void)
+{
+    printf("Exit handler executed for process with PID: %d\n", (int)getpid());
 }
 
-void parent_task() {
-
+void sigint_custom_handler(int sig_num)
+{
+    printf("Process %d: Received SIGINT signal (number %d). Terminating process.\n", (int)getpid(), sig_num);
+    exit(0);
 }
 
-void child_task() {
-    pid_t pid = getpid();
-    int lim = 100;
-    int c = 0;
-    int c_lim = 5;
-    for (int i=0; i<lim; i++) {
-        printf("\r");
-        printf("\e[K");
-        printf("[%d] ", pid);
-        printf("%d/%d ", i, lim);
-        printf("Child working");
-        for (int j=0; j<c+1; j++) {
-            printf(".");
-        }
-        fflush(stdout);
-
-        c++;
-        c = c%c_lim;
-        usleep(100*1000);
-    }
-    printf("\n");
+void sigterm_custom_handler(int sig_num)
+{
+    printf("Process %d: Received SIGTERM signal (number %d). Terminating process.\n", (int)getpid(), sig_num);
+    exit(0);
 }
 
-void atexit_func() {
-    printf("[%d] Exited\n", getpid());
-}
+int main(void)
+{
+    pid_t fork_result;
+    int wait_status;
 
-void onexit_func(int code, void* args) {
-    printf("[%d] Is going to exit with code %d\n", getpid(), code);
-}
-
-void handler_int(int sig) {
-    printf("[%d] RECEIVED SIGINT\n", getpid());
-    exit(sig);
-}
-
-void handler_term(int sig, siginfo_t *info, void *context) {
-    printf("[%d] RECEIVED SIGNTERM\n", getpid());
-    exit(sig);
-}
-
-int main() {
-    atexit(atexit_func);
-    on_exit(onexit_func, NULL);
-
-    struct sigaction act = {0};
-    act.sa_flags = 0;
-    act.sa_sigaction = &handler_term;
-    if (sigaction(SIGTERM, &act, NULL) == -1) {
-        fprintf(stderr, "Cannot sigaction: %s\n", strerror(errno));
-        return 1;
+    if (atexit(process_exit_handler) != 0) {
+        perror("atexit registration failed");
+        exit(EXIT_FAILURE);
     }
 
-    signal(SIGINT, handler_int);
-
-    pid_t pid = fork();
-    if (pid == -1) {
-        fprintf(stderr, "Cannot fork: %s\n", strerror(errno));
-        return 1;
+    if (signal(SIGINT, sigint_custom_handler) == SIG_ERR) {
+        perror("signal for SIGINT failed");
+        exit(EXIT_FAILURE);
     }
 
-    if (pid == 0) {
-        print_pid();
+    struct sigaction sa_struct;
+    memset(&sa_struct, 0, sizeof(sa_struct));
+    sa_struct.sa_handler = sigterm_custom_handler;
+    sigemptyset(&sa_struct.sa_mask);
+    sa_struct.sa_flags = 0;
 
-        child_task();
-        exit(0);
+    if (sigaction(SIGTERM, &sa_struct, NULL) == -1) {
+        perror("sigaction for SIGTERM failed");
+        exit(EXIT_FAILURE);
     }
 
-    print_pid();
+    printf("Parent process started. PID: %d, PPID: %d\n", (int)getpid(), (int)getppid());
 
-    int wstatus = 0;
-    do {
-        if (waitpid(pid, &wstatus, 0) == -1) {
-            fprintf(stderr, "Parent fail waitpid: %s\n", strerror(errno));
-            exit(1);
+    fork_result = fork();
+
+    if (fork_result == -1) {
+        perror("fork system call failed");
+        exit(EXIT_FAILURE);
+    }
+    else if (fork_result == 0) {
+        printf("Child process running. PID: %d, PPID: %d\n", (int)getpid(), (int)getppid());
+        sleep(2);
+        printf("Child process completed\n");
+        exit(42);
+    }
+    else {
+        printf("Parent process: Created child process with PID: %d\n", (int)fork_result);
+
+        if (wait(&wait_status) == -1) {
+            perror("wait system call failed");
+            exit(EXIT_FAILURE);
         }
 
-        if (WIFEXITED(wstatus)) {
-            printf("%d exited with status %d\n", pid, WEXITSTATUS(wstatus));
-        } else if (WIFSIGNALED(wstatus)) {
-            printf("%d killed by signal %d\n", pid, WTERMSIG(wstatus));
-        } else if (WIFSTOPPED(wstatus)) {
-            printf("%d stopped by signal %d\n", pid, WSTOPSIG(wstatus));
-        } else if (WIFCONTINUED(wstatus)) {
-            printf("%d continued\n", pid);
+        if (WIFEXITED(wait_status)) {
+            printf("Parent process: Child exited with status: %d\n", WEXITSTATUS(wait_status));
+        } else if (WIFSIGNALED(wait_status)) {
+            printf("Parent process: Child terminated by signal: %d\n", WTERMSIG(wait_status));
+        } else {
+            printf("Parent process: Child process status unknown\n");
         }
-    } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
 
-    parent_task();
+        printf("Parent process completed\n");
+    }
 
     return 0;
 }
