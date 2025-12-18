603616936393028ec79df5511e4f2c7989b20cb9
Nemkoff <nemkov_work@mail.ru>
2025-12-18 11:07:50 +0300

    refactoring lab1

--- lab1_cat_grep/main.c
+++ lab1_cat_grep/main.c
@@ -1,185 +1,219 @@
-#include <string.h>
-#include <stdio.h>
-#include <getopt.h>
-#include <regex.h>
+#define _GNU_SOURCE  // for getline()
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>  // for getopt_long()
+#include <unistd.h>
+#include <regex.h>
+#include <ctype.h>
 
-int mycat_process_file(const char *file_name, int flags);
-void mycat_process_stdin(int flags);
-int mycat_main(int argc, char *argv[]);
-int mygrep_search_in_file(const char *pattern, const char *file_name, int multiple_files);
-void mygrep_search_in_stdin(const char *pattern);
-int mygrep_main(int argc, char *argv[]);
-enum {
-    NO_FLAGS = 0,  // без флагов
-    N_FLAG = 1,    // флаг -n
-    B_FLAG = 2,    // флаг -b
-    E_FLAG = 4     // флаг -E
-};
-// Вспомогательные функции для устранения дублирования
-static void mycat_print_line(char *line, ssize_t read, int flags, int *line_number) {
-    int is_empty = (line[0] == '\n');
+#define LINE_NUM_FLAG 0x01
+#define NONBLANK_NUM_FLAG 0x02
+#define END_MARKER_FLAG 0x04
 
-    // Нумерация строк
-    if ((flags & B_FLAG) && !is_empty) {
-        printf("%6d\t", (*line_number)++);
-    } else if ((flags & N_FLAG) && !(flags & B_FLAG)) {
-        printf("%6d\t", (*line_number)++);
+typedef struct {
+    int options;
+    int exit_code;
+} AppState;
+
+// ==================== TEXT PROCESSOR (CAT-LIKE) ====================
+void display_text_line(char *text_buffer, size_t length, int options_set, int *counter) {
+    int line_is_empty = (text_buffer[0] == '\n' || length == 0);
+
+    // Handle line numbering
+    if ((options_set & NONBLANK_NUM_FLAG) && !line_is_empty) {
+        printf("%6d\t", (*counter)++);
+    } else if ((options_set & LINE_NUM_FLAG) && !(options_set & NONBLANK_NUM_FLAG)) {
+        printf("%6d\t", (*counter)++);
     }
 
-    // Показ символа конца строки
-    if (flags & E_FLAG) {
-        if (read > 0 && line[read - 1] == '\n') {
-            line[read - 1] = '$';
-            printf("%s\n", line);
+    // Handle end-of-line markers
+    if (options_set & END_MARKER_FLAG) {
+        if (length > 0 && text_buffer[length - 1] == '\n') {
+            text_buffer[length - 1] = '\0';
+            printf("%s$\n", text_buffer);
         } else {
-            printf("%s$\n", line);
+            printf("%s$\n", text_buffer);
         }
     } else {
-        printf("%s", line);
+        printf("%s", text_buffer);
     }
 }
 
-static void mycat_process_stream(FILE *stream, int flags) {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    int line_number = 1;
+void stream_text_display(FILE *input_stream, int options_set) {
+    char *buffer_ptr = NULL;
+    size_t buffer_size = 0;
+    ssize_t bytes_read;
+    int current_line = 1;
 
-    while ((read = getline(&line, &len, stream)) != -1) {
-        mycat_print_line(line, read, flags, &line_number);
+    while ((bytes_read = getline(&buffer_ptr, &buffer_size, input_stream)) != -1) {
+        display_text_line(buffer_ptr, bytes_read, options_set, &current_line);
     }
-    free(line);
+
+    if (buffer_ptr != NULL) free(buffer_ptr);
 }
 
-// Для mygrep: общий поиск по уже скомпилированному регулярному выражению
-static int mygrep_search_stream_with_regex(regex_t *regex, FILE *stream, const char *file_name, int multiple_files) {
-    char *line = NULL;
-    size_t len = 0;
-    int exit_status = 0;
-    while (getline(&line, &len, stream) != -1) {
-        if (regexec(regex, line, 0, NULL, 0) == 0) {
-            if (multiple_files && file_name != NULL) {
-                printf("%s:%s", file_name, line);
-            } else {
-                printf("%s", line);
-            }
-        }
+int process_text_file(const char *filename, int options_set) {
+    FILE *input_file = fopen(filename, "r");
+    if (input_file == NULL) {
+        fprintf(stderr, "Error opening '%s': ", filename);
+        perror("");
+        return 1;
     }
-    free(line);
-    return exit_status;
+
+    stream_text_display(input_file, options_set);
+    fclose(input_file);
+    return 0;
 }
 
+void process_text_input(int options_set) {
+    stream_text_display(stdin, options_set);
+}
 
-int mycat_main(int argc, char *argv[]) {
-    int opt;
-    int flags = NO_FLAGS;
-    int exit_status = 0;
-    struct option long_options[] = {
-            {"number", no_argument, 0, 'n'},
-            {"number-nonblank", no_argument, 0, 'b'},
-            {"show-ends", no_argument, 0, 'E'},
-            {0, 0, 0, 0}
+int text_processor_main(int arg_count, char *arg_values[]) {
+    int current_option;
+    int selected_options = 0;
+    AppState app_state = {0, 0};
+
+    struct option long_opts[] = {
+            {"number", 0, NULL, 'n'},
+            {"number-nonblank", 0, NULL, 'b'},
+            {"show-ends", 0, NULL, 'E'},
+            {NULL, 0, NULL, 0}
     };
-    optind = 1; // Reset getopt
-    while ((opt = getopt_long(argc, argv, "nbE", long_options, NULL)) != -1) {
-        switch (opt) {
+
+    // Reset option index for reusability
+    optind = 1;
+
+    while ((current_option = getopt_long(arg_count, arg_values, "nbE", long_opts, NULL)) != -1) {
+        switch (current_option) {
             case 'n':
-                flags |= N_FLAG;
+                selected_options |= LINE_NUM_FLAG;
                 break;
             case 'b':
-                flags |= B_FLAG;
+                selected_options |= NONBLANK_NUM_FLAG;
                 break;
             case 'E':
-                flags |= E_FLAG;
+                selected_options |= END_MARKER_FLAG;
                 break;
             default:
-                fprintf(stderr, "Usage: %s [-n] [-b] [-E] [file...]\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-n] [-b] [-E] [file...]\n", arg_values[0]);
                 return 1;
         }
     }
-    if (optind == argc) {
-        mycat_process_stdin(flags);
+
+    if (optind == arg_count) {
+        process_text_input(selected_options);
     } else {
-        for (int i = optind; i < argc; i++) {
-            exit_status |= mycat_process_file(argv[i], flags);
+        for (int idx = optind; idx < arg_count; idx++) {
+            app_state.exit_code |= process_text_file(arg_values[idx], selected_options);
         }
     }
-    return exit_status;
+
+    return app_state.exit_code;
 }
 
-int mycat_process_file(const char *file_name, int flags) {
-    FILE *file = fopen(file_name, "r");
-    if (file == NULL) {
-        perror(file_name);
-        return 1;
-    }
-    mycat_process_stream(file, flags);
-    fclose(file);
-    return 0;
-}
+// ==================== PATTERN SEARCHER (GREP-LIKE) ====================
+int search_stream_pattern(regex_t *compiled_regex, FILE *input_stream,
+                          const char *source_name, int multi_source) {
+    char *line_buffer = NULL;
+    size_t buffer_capacity = 0;
+    int found_status = 0;
 
-void mycat_process_stdin(int flags) {
-    mycat_process_stream(stdin, flags);
-}
-
-
-int mygrep_main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s pattern [file...]\n", argv[0]);
-        return 1;
-    }
-    const char *pattern = argv[1];
-    int exit_status = 0;
-    int multiple_files = (argc > 3);
-
-    if (argc == 2) {
-        mygrep_search_in_stdin(pattern);
-    } else {
-        for (int i = 2; i < argc; i++) {
-            exit_status |= mygrep_search_in_file(pattern, argv[i], multiple_files);
+    while (getline(&line_buffer, &buffer_capacity, input_stream) != -1) {
+        if (regexec(compiled_regex, line_buffer, 0, NULL, 0) == 0) {
+            if (multi_source && source_name != NULL) {
+                printf("%s:%s", source_name, line_buffer);
+            } else {
+                printf("%s", line_buffer);
+            }
+            found_status = 1;
         }
     }
-    return exit_status;
+
+    if (line_buffer != NULL) free(line_buffer);
+    return found_status ? 0 : 1;
 }
 
-int mygrep_search_in_file(const char *pattern, const char *file_name, int multiple_files) {
-    FILE *file = fopen(file_name, "r");
-    if (file == NULL) {
-        perror(file_name);
+int search_file_pattern(const char *search_pattern, const char *filename, int multi_source) {
+    FILE *input_file = fopen(filename, "r");
+    if (input_file == NULL) {
+        fprintf(stderr, "Cannot open '%s': ", filename);
+        perror("");
         return 1;
     }
 
-    regex_t regex;
+    regex_t pattern_regex;
+    int compile_result = regcomp(&pattern_regex, search_pattern, REG_EXTENDED);
 
-    if (regcomp(&regex, pattern, REG_EXTENDED)) {
-        fprintf(stderr, "Could not compile regex\n");
-        fclose(file);
+    if (compile_result != 0) {
+        fprintf(stderr, "Pattern compilation failed\n");
+        fclose(input_file);
         return 1;
     }
-    mygrep_search_stream_with_regex(&regex, file, file_name, multiple_files);
-    regfree(&regex);
-    fclose(file);
-    return 0;
+
+    int search_result = search_stream_pattern(&pattern_regex, input_file, filename, multi_source);
+
+    regfree(&pattern_regex);
+    fclose(input_file);
+
+    return search_result;
 }
 
-void mygrep_search_in_stdin(const char *pattern) {
-    regex_t regex;
-    if (regcomp(&regex, pattern, REG_EXTENDED)) {
-        fprintf(stderr, "Could not compile regex\n");
+void search_input_pattern(const char *search_pattern) {
+    regex_t pattern_regex;
+
+    if (regcomp(&pattern_regex, search_pattern, REG_EXTENDED) != 0) {
+        fprintf(stderr, "Pattern compilation failed\n");
         return;
     }
-    mygrep_search_stream_with_regex(&regex, stdin, NULL, 0);
-    regfree(&regex);
+
+    search_stream_pattern(&pattern_regex, stdin, NULL, 0);
+    regfree(&pattern_regex);
 }
 
-int main(int argc, char *argv[]) {
-    if (strstr(argv[0], "mycat")) {
-        return mycat_main(argc, argv);
-    } else if (strstr(argv[0], "mygrep")) {
-        return mygrep_main(argc, argv);
-    } else {
-        fprintf(stderr, "Executable must be named 'mycat' or 'mygrep'\n");
+int pattern_searcher_main(int arg_count, char *arg_values[]) {
+    if (arg_count < 2) {
+        fprintf(stderr, "Usage: %s search_pattern [file...]\n", arg_values[0]);
         return 1;
     }
+
+    const char *target_pattern = arg_values[1];
+    int overall_status = 0;
+    int multiple_sources = (arg_count > 3);
+
+    if (arg_count == 2) {
+        search_input_pattern(target_pattern);
+    } else {
+        for (int idx = 2; idx < arg_count; idx++) {
+            overall_status |= search_file_pattern(target_pattern, arg_values[idx], multiple_sources);
+        }
+    }
+
+    return overall_status;
+}
+
+// ==================== MAIN DISPATCHER ====================
+int main(int arg_count, char *arg_values[]) {
+    char *exec_name = arg_values[0];
+
+    // Case-insensitive check for program name
+    char *lower_name = strdup(exec_name);
+    for (int i = 0; lower_name[i]; i++) {
+        lower_name[i] = tolower(lower_name[i]);
+    }
+
+    int result = 1;
+
+    if (strstr(lower_name, "mycat") != NULL) {
+        result = text_processor_main(arg_count, arg_values);
+    } else if (strstr(lower_name, "mygrep") != NULL) {
+        result = pattern_searcher_main(arg_count, arg_values);
+    } else {
+        fprintf(stderr, "Program must be executed as 'mycat' or 'mygrep'\n");
+    }
+
+    free(lower_name);
+    return result;
 }
